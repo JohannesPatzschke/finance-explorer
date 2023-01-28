95033e3d65de0b646a2f4a01bfbcbe8792091bbe
Johannes Patzschke <johannes.patzschke@gmail.com>
2023-01-28 22:00:47 +0100

    feat: Settings setup with theme toggle

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -1,7 +1,47 @@
 import React from 'react';
+import {
+  Heading,
+  Divider,
+  Card,
+  CardBody,
+  Text,
+  Flex,
+  Center,
+  Box,
+  Switch,
+  useColorMode,
+} from '@chakra-ui/react';
 
 const Settings = () => {
-  return <div>Settings</div>;
+  const { colorMode, toggleColorMode } = useColorMode();
+
+  return (
+    <>
+      <Heading size="lg">Settings</Heading>
+      <br />
+      <Heading size="md" mb={2}>
+        General
+      </Heading>
+      <Divider mb={4} />
+      <Card variant="outline">
+        <CardBody>
+          <Flex color="white">
+            <Center w="150px">
+              <Switch
+                colorScheme="teal"
+                isChecked={colorMode === 'dark'}
+                onChange={toggleColorMode}
+              />
+            </Center>
+            <Box flex="1">
+              <Text as="b">Dark Mode</Text>
+              <Text fontSize="sm">Toggle dark mode</Text>
+            </Box>
+          </Flex>
+        </CardBody>
+      </Card>
+    </>
+  );
 };
 
 export default Settings;
