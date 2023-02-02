f4015f9b020501e43f5b83917053927daa9bcffd
Johannes Patzschke <johannes.patzschke@gmail.com>
2023-02-02 09:28:39 +0100

    refactor: Explore charts

--- src/pages/Explore.tsx
+++ src/pages/Explore.tsx
@@ -16,6 +16,7 @@ import useFilteredTransactions from '@hooks/useFilteredTransactions';
 import useCategories from '@hooks/useCategories';
 import { TransactionType } from '@models/Transaction';
 import { toCurrency } from '@utils/currency';
+import { CategoryType } from '@models/Category';
 
 const COLOR_PALETTE = [
   '#FC8181',
@@ -31,26 +32,49 @@ const COLOR_PALETTE = [
 
 const DAYS_PER_MONTH = 30.4375;
 
-function calculatePieData(transactions: Array<TransactionType>) {
+function calculatePieData(
+  transactions: Array<TransactionType>,
+  categories: Array<CategoryType & { fill: string }>,
+) {
   const categoryGroups = groupBy(transactions, ({ categoryId }) => categoryId ?? 'Uncategorized');
 
-  return Object.entries(categoryGroups).map(([key, values], index) => {
+  return Object.entries(categoryGroups).map(([categoryId, values]) => {
     const category =
-      key === 'Uncategorized' ? key : useCategories.getState().getCategoryName(key).category;
+      categoryId === 'Uncategorized'
+        ? { name: categoryId, fill: '#CBD5E0' }
+        : categories.find(({ id }) => id === categoryId) ?? { name: categoryId, fill: '#CBD5E0' };
     const value = values.reduce((sum, { amount }) => sum + amount, 0);
 
     return {
-      name: category,
+      name: category.name,
       value: Math.abs(Math.round(value * 100) / 100),
-      fill: COLOR_PALETTE[index],
+      fill: category.fill,
     };
   });
 }
 
 const Explore = () => {
   const transactions = useFilteredTransactions();
+  const categories = useCategories((state) => state.categories).map((category, index) => ({
+    ...category,
+    fill: COLOR_PALETTE[index],
+  }));
 
-  const pieData = calculatePieData(transactions);
+  const legendPayload = categories.map(({ id, name, fill }) => ({
+    id,
+    value: name,
+    type: 'square' as 'square',
+    color: fill,
+  }));
+
+  const incomePieData = calculatePieData(
+    transactions.filter(({ amount }) => amount >= 0),
+    categories,
+  );
+  const outcomePieData = calculatePieData(
+    transactions.filter(({ amount }) => amount < 0),
+    categories,
+  );
 
   const diffDays = dayjs
     .unix(transactions[0].timestamp)
@@ -102,14 +126,16 @@ const Explore = () => {
       <br />
       <br />
       <ResponsiveContainer height={500}>
-        <PieChart height={100}>
+        <PieChart height={500}>
           <Pie
             dataKey="value"
             isAnimationActive={false}
-            data={pieData}
-            cx="40%"
+            data={incomePieData}
+            cx="20%"
             cy="25%"
             outerRadius={100}
+            innerRadius={40}
+            paddingAngle={5}
             fill="#8884d8"
             label={({ value, x, y, fill, textAnchor }) => (
               <text x={x} y={y} fill={fill} textAnchor={textAnchor}>
@@ -117,7 +143,35 @@ const Explore = () => {
               </text>
             )}
           />
-          <Legend verticalAlign="top" height={36} layout="vertical" align="right" />
+          <text x={225} y={130} fill="#ffffff" textAnchor="middle" dominantBaseline="middle">
+            Income
+          </text>
+          <Pie
+            dataKey="value"
+            isAnimationActive={false}
+            data={outcomePieData}
+            cx="60%"
+            cy="25%"
+            outerRadius={100}
+            innerRadius={40}
+            paddingAngle={5}
+            fill="#8884d8"
+            label={({ value, x, y, fill, textAnchor }) => (
+              <text x={x} y={y} fill={fill} textAnchor={textAnchor}>
+                {toCurrency(value)} €
+              </text>
+            )}
+          />
+          <text x={670} y={130} fill="#ffffff" textAnchor="middle" dominantBaseline="middle">
+            Outcome
+          </text>
+          <Legend
+            verticalAlign="top"
+            height={36}
+            layout="vertical"
+            align="right"
+            payload={legendPayload}
+          />
         </PieChart>
       </ResponsiveContainer>
     </>
